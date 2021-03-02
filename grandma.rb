1fad7af8c009182d7a783051e9d330be945e276e
Jesse Wenger <wengerj@gmail.com>
2021-03-02 22:40:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(response)
-  puts response
+def speak_to_grandma(phrase)
+  phrase.upcase?
+  
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
